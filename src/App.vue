6550ef177b25f398c54bc4a1f84701d0f21e753b
Pablo <klabitos@usal.es>
2023-03-17 17:14:10 +0100

    [INIT] start project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p class="test">test</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+ .test{
+  border: 1px solid black;
+  padding: 15px;
+ }
+</style>
