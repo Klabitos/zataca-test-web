efa61e35cb1b3af8238a36a5801f86ee301f85a4
Pablo <klabitos@usal.es>
2023-03-18 03:08:49 +0100

    [ADD] title filter

--- src/components/inputs/SearchBar.vue
+++ src/components/inputs/SearchBar.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="search-bar-container">
-        <input v-model="inputText" class="search-bar" type="text" placeholder="Search a title or description...">
+        <input v-model="inputText" class="search-bar" @keyup="filterTextChanged" type="text"
+            placeholder="Search a title or description...">
         <div class="search-bar-options">
             <input v-model="isTitleActive" disabled type="checkbox"> Title
             <input v-model="isDescriptionActive" type="checkbox"> Description
@@ -8,6 +9,7 @@
     </div>
 </template>
 <script>
+import Vue from 'vue';
 export default {
     name: "SearchBar",
     data() {
@@ -16,25 +18,34 @@ export default {
             isTitleActive: true,
             isDescriptionActive: false
         }
+    },
+    created() {
+        Vue.prototype.$globals = Vue.observable({
+            filter: ''
+        });
+    },
+    methods: {
+        filterTextChanged() {
+            this.$globals.filter =  this.inputText;
+        }
     }
+
 }
 </script>
 <style lang="scss" scoped>
-
 .search-bar {
     width: 100%;
     padding: 15px;
     border-radius: 20px;
 }
 
-.search-bar-options{
+.search-bar-options {
     margin-top: 0.5rem;
 }
 
 @media (max-width:550px) {
-    .search-bar-options{
+    .search-bar-options {
         display: none;
     }
 }
-
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,34 @@
 <template>
     <div>
-        <StardardCardGallery :items="allItemsData" />
+        <StardardCardGallery v-if="allItemsData.length > 0" :items="getFilteredItems" />
     </div>
 </template>
 <script>
 import { getItemsData } from '@/service/defaultItems';
 import StardardCardGallery from '@/components/cardsGallery/StandardCardGallery.vue';
+
 export default {
     name: 'Home-View',
-    components:{
+    components: {
         StardardCardGallery
     },
-    data(){
-        return{
-            allItemsData:[]
+    data() {
+        return {
+            allItemsData: [],
+            itemsFiltered: [],
         }
     },
-    created(){
-        getItemsData().then(result =>{
+    created() {
+        getItemsData().then(result => {
             this.allItemsData = result.data
         })
+    },
+    computed:{
+        getFilteredItems(){
+            return this.allItemsData.filter(item => item.title.includes(this.$globals.filter))
+        }
     }
+
 }
 </script>
-<style lang="scss" scoped>
-    
-</style>
+<style lang="scss" scoped></style>
