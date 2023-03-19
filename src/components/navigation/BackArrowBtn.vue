3dad415ff68940197da060058e4007f3dce4f741
Pablo <klabitos@usal.es>
2023-03-19 16:37:28 +0100

    [MOD] refactoring back arrow home

---
+++ src/components/navigation/BackArrowBtn.vue
@@ -0,0 +1,51 @@
+<template>
+    <router-link to="/">
+        <div class="go-back-container">
+            <button class="go-back-container--btn">
+                <i class="fa-sharp fa-solid fa-arrow-left"></i>
+
+            </button>
+            <h4 class="go-back-message">{{ $t('components.home.home') }}</h4>
+        </div>
+    </router-link>
+</template>
+<script>
+export default {
+    name: "BackArrowBtn"
+}
+</script>
+<style lang="scss" scoped>
+    .go-back-container{
+        position: relative;
+        left: -100px;
+        max-width: 130px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        transition: 2s;
+        cursor: pointer;
+    }
+    .go-back-container--btn{
+        border: 1px solid var(--primary-footer-color);
+        background-color: var(--primary-yellow-color) !important;
+        border-radius: 25px;
+        padding: 15px;
+        margin: 20px;
+        cursor: pointer;
+
+    }
+
+    .go-back-message{
+        transition: 1s;
+        opacity: 0;
+        cursor: pointer;
+
+    }
+
+    .go-back-container:hover {
+        max-width: 150px;
+    }
+
+    .go-back-container:hover .go-back-message{
+        opacity: 1;
+    }</style>
--- src/views/CardDetail.vue
+++ src/views/CardDetail.vue
@@ -1,14 +1,6 @@
 <template>
     <div>
-        <router-link to="/">
-            <div class="go-back-container">
-                <button class="go-back-container--btn">
-                    <i class="fa-sharp fa-solid fa-arrow-left"></i>
-                    
-                </button>
-                <h4 class="go-back-message">{{$t('components.home.home')}}</h4>
-            </div>
-        </router-link>
+        <BackArrowBtn></BackArrowBtn>
         <h3 v-if="item=='loading'">{{$t('components.data.being-loaded')}}</h3>
         <DetailCard v-else-if="item!=''" :itemInfo="item"></DetailCard>
         <h3 v-if="item==null">{{$t('components.data.no-detail-data')}}</h3>
@@ -17,11 +9,12 @@
 </template>
 <script>
 import { getItemDataById } from '@/service/defaultItems';
+import BackArrowBtn from '@/components/navigation/BackArrowBtn.vue';
 import DetailCard from '@/components/cards/DetailCard.vue';
 export default {
     name:"CardDetail",
     components:{
-        DetailCard
+        DetailCard, BackArrowBtn
     },
     data(){
         return{
@@ -38,38 +31,5 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-    .go-back-container{
-        position: relative;
-        left: -100px;
-        max-width: 130px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        transition: 2s;
-        cursor: pointer;
-    }
-    .go-back-container--btn{
-        border: 1px solid var(--primary-footer-color);
-        background-color: var(--primary-yellow-color) !important;
-        border-radius: 25px;
-        padding: 15px;
-        margin: 20px;
-        cursor: pointer;
 
-    }
-
-    .go-back-message{
-        transition: 1s;
-        opacity: 0;
-        cursor: pointer;
-
-    }
-
-    .go-back-container:hover {
-        max-width: 150px;
-    }
-
-    .go-back-container:hover .go-back-message{
-        opacity: 1;
-    }
 </style>
